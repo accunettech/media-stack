7e177b2f82fa6f20d703cfd6bb9bea2c0536a761
accunettech <chrispilotto@gmail.com>
2025-08-10 19:20:10 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
-echo "Creating config directories in current location. This should be the location set for CONTAINER_HOME env var..."
+echo "Setting up python virtual environment..."
+python3 -m venv venv
+
+echo "Adding python libraries..."
+pip install -r requirements.txt
+
+echo "Creating config directories..."
 mkdir -p ./conf/{gluetun,qbittorrent,jellyfin,sonarr,radarr,prowlarr}
 
 docker compose up -d
@@ -7,31 +13,27 @@ python scripts/config_stack.py
 
 cat << EOF
 App config tips (once)
-    qBittorrent:
-        URL: http://{docker-host}:8080/
-        Settings → Downloads
-            Default Save Path: /downloads
-            Category paths:
-                radarr → /downloads
-                sonarr → /downloads
+    qBittorrent: http://$HOSTNAME:8080/
 
-    Sonarr:
-        URL: http://{docker-host}:8989/
+    Sonarr: http://$HOSTNAME:8989/
         Root folder: /shows
         Download client category: sonarr
         Completed Download Handling: enabled
+        Set priority of Usenet indexers higher (lower number) to favor Usenet
 
-    Radarr:
-        URL: http://{docker-host}:7878/
+    Radarr: http://$HOSTNAME:7878/
         Root folder: /movies
         Download client category: radarr
         Completed Download Handling: enabled
+        Set priority of Usenet indexers higher (lower number) to favor Usenet
 
-    Jellyfin:
-        URL: http://{docker-host}:8096 (or https://{docker-host}:8920 if SSL enabled)
+    Jellyfin: http://$HOSTNAME:8096/
         Libraries: Movies → /media/movies, Shows → /media/shows
         Hardware Transcoding: enable VAAPI and select /dev/dri/renderD128 (if using device that support VAAPI)
 
-    Prowlarr:
-        URL: http://{docker-host}:9696/
+    Prowlarr: http://$HOSTNAME:9696/
+        If using usenet, set priority low and set torrent indexers high
+
+    sabnzbd: http://$HOSTNAME:8081/
+        Setup Downloader with API key (from sabnzbd setup) in Sanarr and Radarr if Usenet will be used
 EOF
